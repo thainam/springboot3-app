ff4bf8985ad621ba27f8681a41cacf3dc10695b7
Thainam Zorzal <thainam1@hotmail.com>
2024-10-08 16:00:17 -0300

    Adds CRUD of User

--- src/main/java/com/course/hibernate/resources/UserResource.java
+++ src/main/java/com/course/hibernate/resources/UserResource.java
@@ -1,15 +1,22 @@
 package com.course.hibernate.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import com.course.hibernate.entities.User;
 import com.course.hibernate.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -29,4 +36,24 @@ public class UserResource {
         User user = userService.findById(id);
         return ResponseEntity.ok(user);
     }
+
+    @PostMapping
+    public ResponseEntity<User> save(@RequestBody User user) {
+        user = userService.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+        return ResponseEntity.ok(userService.update(id, user));
+    }
+
 }
--- src/main/java/com/course/hibernate/services/UserService.java
+++ src/main/java/com/course/hibernate/services/UserService.java
@@ -19,7 +19,27 @@ public class UserService {
     }
 
     public User findById(Long id) {
-        Optional<User> user = repository.findById(Math.toIntExact(id));
+        Optional<User> user = repository.findById(id.intValue());
         return user.get();
+    }
+
+    public User insert(User user) {
+        return repository.save(user);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(Math.toIntExact(id));
+    }
+
+    public User update(Long id, User user) {
+        User entity = repository.getReferenceById(id.intValue());
+        updateData(entity, user);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User user) {
+        entity.setName(user.getName());
+        entity.setEmail(user.getEmail());
+        entity.setPhone(user.getPhone());
     }
 }
