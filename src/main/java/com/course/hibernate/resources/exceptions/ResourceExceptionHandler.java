ec0b83e302ccdd5244c176e66c8cd3ad62724159
Thainam Zorzal <thainam1@hotmail.com>
2024-10-08 16:29:50 -0300

    Adds Exceptions to user endpoints.

---
+++ src/main/java/com/course/hibernate/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.course.hibernate.resources.exceptions;
+
+import java.time.Instant;
+
+import com.course.hibernate.services.exceptions.DatabaseException;
+import com.course.hibernate.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        String error = "Resource not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> databaseException(DatabaseException e, HttpServletRequest request) {
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
--- src/main/java/com/course/hibernate/services/UserService.java
+++ src/main/java/com/course/hibernate/services/UserService.java
@@ -5,7 +5,12 @@ import java.util.Optional;
 
 import com.course.hibernate.entities.User;
 import com.course.hibernate.repositories.UserRepository;
+import com.course.hibernate.services.exceptions.DatabaseException;
+import com.course.hibernate.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,7 +25,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> user = repository.findById(id.intValue());
-        return user.get();
+        return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user) {
@@ -28,13 +33,24 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(Math.toIntExact(id));
+        try {
+            repository.deleteById(Math.toIntExact(id));
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("It's not possible to delete an user that has orders.");
+        }
     }
 
     public User update(Long id, User user) {
-        User entity = repository.getReferenceById(id.intValue());
-        updateData(entity, user);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id.intValue());
+
+            updateData(entity, user);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User user) {
